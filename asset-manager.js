f55e4eb2e25236c402293e081a94fbd191009e2d
ninjaflamingo-2025 <moststripesmedia@gmail.com>
2025-08-22 17:35:56 +0200

    Update asset-manager.js

--- asset-manager.js
+++ asset-manager.js
@@ -129,6 +129,8 @@ class AssetManager extends EventTarget {
     this.addEventListener(event, listener);
   }
 }
-export const asset_manager = new AssetManager(ASSET_STORE);
+
+const asset_manager = new AssetManager(ASSET_STORE);
+export { asset_manager };
 
 
